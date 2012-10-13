3300531cd11a1fd51e026ed2b2a10e1f72aec055
Meritas <bubobuboster@gmail.com>
2012-10-13 22:50:42 +0300

    the night is is almost here

---
+++ AirPlane.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class AirPlane {
+public void printSeats() {
+int[][] seats = new int[6][27];
+for (int r=0;r<=26;r++) {
+for(int c=0;c<=5;c++) {
+System.out.print(seats[c][r]+ " ");
+if (c==2)
+System.out.print(" ");
+}
+System.out.println(" ");
+}
+
+}
+public static void main(String[] args) {
+AirPlane plane = new AirPlane();
+plane.printSeats();
+int sum=0;
+for (int i=0;sum<162;i++) {
+int rvalue = new Random().nextInt(3) +1;
+if (sum+rvalue<=162) {
+sum+=rvalue;
+}
+}
+System.out.println(sum);
+
+}
+}
