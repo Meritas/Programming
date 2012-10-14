2b9315b6046afeed814cddcddf413f761b72608a
Meritas <bubobuboster@gmail.com>
2012-10-14 16:19:14 +0300

    lele

--- AirPlane.java
+++ AirPlane.java
@@ -2,39 +2,44 @@ import java.util.*;
 
 public class AirPlane {
 	
-	public void flyingVehicle(){
+	public class flyingVehicle{
+		int sum = 0;
 		int[][] seats = new int[27][6];
-		fillingRobot Filler = new fiillingRobot;
+		fillingComputer Filler = new fillingComputer();
 		public void add_seats(){
-			for(int i=0; int sum<162; i++){
+			for(int i=0; sum<162; i++){
 				int rvalue = new Random().nextInt(Filler.al_c) + 1;//al_c -> highest case allowed, every lower case than the current highest case is allowed by default)
 				seat_case(rvalue);
-				
-				 }
+			}
+		}
+	}
 
 
-	public void fillingRobot(num){//num == rvalue/num of case
+	public class fillingRobot{
 			int cRow = 0;
-			int al_c; 
+			int al_c;
+			int num = 3;//num == rvalue/num of case
 			boolean iftrue = false;
 			
-			switch(num){
-				case 1: case1(0);
-						break;
-				case 2: case2(0);
-						break;
-				case 3: case3(0);
-						break;
-				case 999: System.out.println("No more free seats.");
-						break;
+			case_system(int num){
+				switch(num){
+					case 1: case1(0);
+					break;
+					case 2: case2(0);
+					break;
+					case 3: case3(0);
+					break;
+					case 999: System.out.println("No more free seats.");
+					break;
+				}
 			}
 
-			case1(int i){
-				for( i=0; i<=5; i=i+3 ){
+			case1(){
+				for( int i=0; i<=5; i=i+3 ){
 					if (seats[cRow][i]+seats[cRow][i+2]=0){
-						seats[cRow[i] = 1;
-						seats[cRow[i+1] = 1;
-						seats[cRow[i+2] = 1;
+						seats[cRow][i] = 1;
+						seats[cRow][i+1] = 1;
+						seats[cRow][i+2] = 1;
 						iftrue = true;
 						break;
 						} else {
@@ -42,7 +47,7 @@ public class AirPlane {
 						}
 					}
 					if ( iftrue == false ){
-						if ( cRow == 5  ){
+						if ( cRow == 27  ){
 						al_c = 2;
 						} else {
 						cRow = cRow + 1;
@@ -52,8 +57,8 @@ public class AirPlane {
 						}
 				}
 
-			case2(int i){
-				for ( i=0; i<=5; i=i+1 ){
+			case2(){
+				for ( int i=0; i<=5; i=i+1 ){
 					if(i==2) continue;
 					if(seats[cRow][i]+seats[cRow][i+1] == 0){
 						seats[cRow][i] = 1;
@@ -61,7 +66,7 @@ public class AirPlane {
 						iftrue = true;
 						}
 					if ( iftrue == false ){
-						if ( cRow == 5  ){
+						if ( cRow == 27  ){
 						al_c = 1;
 						} else {
 						cRow = cRow + 1;
@@ -70,41 +75,35 @@ public class AirPlane {
 						}
 			}
 
-			case3(int i){
-				for( i=0; i<=5; i=i+1 ){
-					if (seats[cRow][i] == 0){
-						seats[cRow][i] == 1)
+			case3(){
+				for( int i=0; i<=5; i=i+1 ){
+					if ((seats[cRow][i]) == 0){
+						seats[cRow][i] = 1)
 						iftrue = true;
 						}
 					}
 					if ( iftrue==false ){
-						if ( cRow == 5  ){
+						if ( cRow == 27  ){
 						al_c = 999;
 						} else {
 						cRow = cRow+1;
 						seats[cRow][0] = 1;
-					}
+						}
 
 			}
+		}
 		
 				
 
 	
 public static void main(String[] args) {
 		AirPlane plane = new AirPlane();
-		plane.printSeats();
-		int sum=0;
-		for (int i=0;sum<162;i++) {
-			int rvalue = new Random().nextInt(3) +1;
-			if (sum+rvalue<=162) {
-				sum+=rvalue;
-				}
-			}
-		System.out.println(sum);
-
 		}
-	}
 
+
+
+
+}
 
 
 // BEWARE THRASH/UNUSED CODE BELOW!
