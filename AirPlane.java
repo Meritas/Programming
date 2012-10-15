265418611791f13a201d9964a93c7b8a26622c34
Meritas <bubobuboster@gmail.com>
2012-10-15 04:41:13 +0300

    E RAZGELE! DRUJ!

--- AirPlane.java
+++ AirPlane.java
@@ -32,7 +32,7 @@ public class AirPlane {
 				Filler.case_system(rvalue);
 				if ( Filler.al_c != al_c2 ) sum = sum - rvalue;
 				System.out.println(" ");
-				System.out.println( rvalue + " passenger(s) enter the airplane." );
+				System.out.println( rvalue + " passenger(s) enters the airplane." );
 				print_seats();
 			}
 
@@ -172,92 +172,4 @@ public class AirPlane {
 						if ( cRow == 26  ) {al_c = 999; return;}
 					}
 			}
-		}
-
-				
-
-	
-
-
-
-
-
-
-
-// BEWARE THRASH/UNUSED CODE BELOW!
-
-	/*public void seat_case(num){//num == rvalue/num of case
-			switch(num){
-				case 1:	seats[cRow][apc.c1_c] = 1;
-					seats[cRow][apc.c1_c+1] = 1;	
-					seats[cRow][apc.c1_c+2] = 1;
-					if( apc.c_r
-					break;
-
-				case 2: seats[apc.c2_r][apc.c2_c] = 1;
-					seats[apc.c2_r][apc.c2_c+1] = 1;
-					break;
-
-				case 3: seats[apc.c3_r][apc.c3_c] = 1;
-					break
-				}
-			}	
--//////////////////////////////////////////////////////////////////////////////////////////////////-
-
-				for( int i3 = 0; i3<=3; i3++){
-					for(int k3 = 0; k3<=5; k3++){
-						if( seats[i3][k3] == 0 ){
-							seats[i3][k3]=1;
-							return;
-						}
-					}
-				cRow++;
-				}
-				al_c = 999;
-				return;
-			}
-		
-
-
--////////////////////////////////////////////////////////////////////////////////////////////////////-
-
-
-*/ 
-
-
-
-/* Advanced mode
-
-int [] paths = new int[num]; //puteki, na vsqka puteka ima n-broy mesta
-
-//commit-needed change-comment
-
-
-for( int row = current_row; boolean found_spots == true; row = row + 1; ){
-
-for (i=0; i<=paths; i = i + 1){
-	found_spots = false;
-	tkn_seats = 0;	//kolko mesta sa zaeti na reda, koito preglejdame
-	point = 0;      //pokazalec do koq sedalka sme stignali
-	if ( paths[i] < ppl ){
-		point = point + paths[i];		
-		continue;
-		}
-
-	for ( k=0; k <= paths[i]; k = k + 1){
-		if( seats[row][point] == 1 ) tkn_seats = tkn_seats + 1;
-		point = point + 1;
-		}
-
-	if ( paths[i] - tkn_seats < ppl ){
-		point = point + (paths[i] - tkn_seats);
-		continue;
-		}
-
-	for ( k2 = 0; k <= ppl; k = k + 1 ){
-		seats[row][point] = 1;
-		point = point + 1;
-		found_spots = true;
-		}
-
-}*/
+		}	
